86564b68f636f601afb488f38a628f07b0e18dc0
tkoya9 <trinadh@dotlabs.com>
2016-08-03 16:32:25 +0530

    open tok video calling

---
+++ app/src/main/java/com/example/trina/gettingstartedopentok/OpenTokConfig.java
@@ -0,0 +1,19 @@
+package com.example.trina.gettingstartedopentok;
+
+public class OpenTokConfig {
+
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+    // Replace with a generated Session ID
+    public static final String SESSION_ID = "1_MX40NTYzMTMzMn5-MTQ3MDIwNzU3Njk4OH5CVjFwcWQ0UmY3N1dYRlJBZFpDbGxnekZ-fg";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTYzMTMzMiZzaWc9MTQyMThmZjkxZmQyMDEzNWQ4ZDcyN2I3OTI5YjVjNzE4MmU1MGZjMDpzZXNzaW9uX2lkPTFfTVg0ME5UWXpNVE16TW41LU1UUTNNREl3TnpVM05qazRPSDVDVmpGd2NXUTBVbVkzTjFkWVJsSkJaRnBEYkd4bmVrWi1mZyZjcmVhdGVfdGltZT0xNDcwMjA3NjA1Jm5vbmNlPTAuNzY2OTI0NzYwNzcwMDUyNyZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDcyNzk5NjExJmNvbm5lY3Rpb25fZGF0YT1IaSUyME9wdGlvbmFsJTIwRGF0YSUyMCEhdGhpcyUyMGlzJTIwd2hlcmUlMjBpdCUyMGlzJTIwY29tbWluZw==";
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "45631332";
+
+    // Subscribe to a stream published by this client. Set to false to subscribe
+    // to other clients' streams only.
+    public static final boolean SUBSCRIBE_TO_SELF = true;
+
+
+}
